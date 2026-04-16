4f130ab661b28ea3514be7159e319b8ff54356de
Darko <darkosemail428@gmail.com>
2026-04-16 05:11:49 -0400

    Set addon and overlay version to 0.3.0

--- SimplyMidnight.lua
+++ SimplyMidnight.lua
@@ -3,7 +3,7 @@ local addonName, addon = ...
 _G.SimplyMidnight = addon
 
 addon.name = addonName
-addon.version = "0.969.99"
+addon.version = "0.3.0"
 addon.constants = {
     questionMarkIcon = 134400,
     slotOrder = { "primary", "secondary", "defensive", "interrupt", "utility" },
